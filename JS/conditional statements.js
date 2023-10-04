a43ce45af28bfdcf437d8d0e93c72e683a24f2fe
Ojas Patil <oja2001pat@gmail.com>
2023-10-05 00:24:34 +0530

    conditional stsatements

---
+++ JS/conditional statements.js
@@ -0,0 +1,43 @@
+// if statement
+// console.log("code before if statement");
+// let age = 3;
+// if (age >= 18) {
+//     console.log("Yay! you can vote")
+//     console.log("Yay! you can drive too.")
+// }
+// if (age < 18) {
+//     console.log("Sorry! you cannot vote")
+// }
+
+// let firstName = "Ojas"
+// if (firstName == "Ojas") {
+//     console.log(`Welcome to the world ${firstName}.`)
+// }
+// console.log("code after if statement");
+
+
+
+
+// // esle-if statement
+// let marks = 35;
+// if (marks >= 80) {
+//     console.log("A++")
+// }
+// else if (marks >= 60 ) {
+//     console.log("A")
+// }
+// else if (marks >= 32 ) {
+//     console.log("B")
+// }
+// else if (marks >= 10 ) {
+//     console.log("F")
+// }
+
+
+// else statement
+// let age = 17;
+// if (age>=18) {
+//     console.log("you can vote")
+// } else {
+//     console.log("you cannot vote")
+// }
