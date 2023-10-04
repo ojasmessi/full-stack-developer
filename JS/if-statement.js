215fbc1f598ae163a149845cadd3ccc4b3a2b786
Ojas Patil <oja2001pat@gmail.com>
2023-10-04 23:51:31 +0530

    if statement

---
+++ JS/if-statement.js
@@ -0,0 +1,15 @@
+console.log("code before if statement");
+let age = 3;
+if (age >= 18) {
+    console.log("Yay! you can vote")
+    console.log("Yay! you can drive too.")
+}
+if (age < 18) {
+    console.log("Sorry! you cannot vote")
+}
+
+let firstName = "Ojas"
+if (firstName == "Ojas") {
+    console.log(`Welcome to the world ${firstName}.`)
+}
+console.log("code after if statement");
