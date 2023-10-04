8ee093728cd98a7547c6888feca4521477f97d95
Ojas Patil <oja2001pat@gmail.com>
2023-10-04 23:52:12 +0530

    operators

--- JS/operators.js
+++ JS/operators.js
@@ -1,17 +1,17 @@
-let age = 18;
-// let a = 10;
-// let b = 5;
+// let age = 18;
+// // let a = 10;
+// // let b = 5;
 
-// Arithmetic operations
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-console.log(a**b);
-// Unary operators
-console.log(a++);
-console.log(++a);
+// // Arithmetic operations
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// // Unary operators
+// console.log(a++);
+// console.log(++a);
 
-// Comparison operators
-console.log(age<=18);
+// // Comparison operators
+// console.log(age<=18);
