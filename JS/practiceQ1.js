7ef4f018c7d8faa90876d702d6ab4013cfec5a40
Ojas Patil <oja2001pat@gmail.com>
2023-10-05 00:25:27 +0530

    update traffic light popcorn price template literals

---
+++ JS/practiceQ1.js
@@ -0,0 +1,13 @@
+// // Traffic light system
+// let color = "red";
+
+// if (color== "red") {
+//     console.log("Stop!")
+// }
+// if (color== "yellow") {
+//     console.log("Slow down!")
+// }
+// if (color== "green") {
+//     console.log("Go!")
+// }
+// console.log("Have a safe and happy jorney")
--- JS/template-literals.js
+++ JS/template-literals.js
@@ -1,15 +1,15 @@
-// console.log("Hello world!");
-// console.log("Ojas Patil");
-let a = 10;
-let b = 5;
-console.log("sum is" , a+b);
+// // console.log("Hello world!");
+// // console.log("Ojas Patil");
+// let a = 10;
+// let b = 5;
+// console.log("sum is" , a+b);
 
-let pencilPrice = 5;
-let eraserPrice = 15;
-// "The total price is:" + (pencilPrice+eraserPrice) + " Rupees.";
-// ` : back tick
-let output = `The total price is: ${pencilPrice+eraserPrice} Rupees.`;
-console.log(output);
+// let pencilPrice = 5;
+// let eraserPrice = 15;
+// // "The total price is:" + (pencilPrice+eraserPrice) + " Rupees.";
+// // ` : back tick
+// let output = `The total price is: ${pencilPrice+eraserPrice} Rupees.`;
+// console.log(output);
 
 
 
