4b861fe352f1fc3d0480a770f21364e5161db773
Ojas Patil <oja2001pat@gmail.com>
2023-10-05 23:05:11 +0530

    Q4

--- JS/practiceQ4.js
+++ JS/practiceQ4.js
@@ -1,8 +1,77 @@
+//Q1 (divisible by 10)
 let num = 10;
 
-if (num%10 == 0) {
-    console.log("good")
+if (num % 10 == 0) {
+    console.log("good");
 } else {
-    console.log("bad")
+    console.log("bad");
 }
 
+
+//Q2 (prompt msg)
+// let name = prompt("enter your name");
+// let age = prompt("enter your age");
+
+// alert(` ${name} is ${age} years old.`)
+
+//Q3 (switch statement)
+let Quarter = 6;
+
+switch (Quarter) {
+    case 1:
+        console.log("jan, feb, march")
+        break;
+    case 1:
+        console.log("april, may, june")
+        break;
+    case 3:
+        console.log("july, aug, sep")
+        break;
+    case 4:
+        console.log("oct, nov , dec")
+        break;
+
+    default: console.log("sorry out of months")
+        break;
+}
+
+//Q4
+let str = "aolden string";
+
+if ((str[0] == 'A') || (str[0] == 'a') && (str.length > 5)) {
+    console.log("it is golden");
+} else {
+    console.log("it is not golden");
+}
+
+//Q5
+let a = 5;
+let b = 18;
+let c = 13;
+
+if(a > b) {
+    if(a > c) {
+        console.log(a, "is largest");
+    } else {
+        console.log(c, "is largest");
+    }
+}
+else {
+        if (b > c) {
+            console.log(b, "is largest")
+        }
+        else {
+            console.log(c, "is largest")
+        }
+    }
+
+
+//Q6
+let num1 = 32;
+let num2 = 47852;
+
+if ( (num1%10) == (num2%10) ) {
+    console.log("numbers have same last digit which is" , num1%10);
+} else {
+    console.log("numbers dont have same last digit which is" , num2%10);
+}
