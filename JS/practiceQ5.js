d6165b1e1779cdd3b253f839061767de2b104640
Ojas Patil <oja2001pat@gmail.com>
2023-10-06 00:11:01 +0530

    updated

---
+++ JS/practiceQ5.js
@@ -0,0 +1,16 @@
+// //Q1
+// let msg = "help!";
+
+// console.log(msg.trim().toUpperCase())
+
+// //Q2
+// let naMe = "ApnaCollege"
+
+// name.slice(4,9);
+// name.indexOf("na")
+// name.replace("Apna" , "Our")
+
+// console.log(name);
+
+// name.slice(4).replace("l", "t").replace("l", "t");
+// console.log(name);
