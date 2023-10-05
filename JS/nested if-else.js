fedbf9c19722907904b91cc39423785be795be5d
Ojas Patil <oja2001pat@gmail.com>
2023-10-05 20:08:35 +0530

    nested-if-else Q2 Q3 Q4

---
+++ JS/nested if-else.js
@@ -0,0 +1,13 @@
+// // Nested if else statement
+// let marks = 45;
+
+// if (marks >= 33) {
+//     console.log("Pass");
+//     if (marks >= 80) {
+//         console.log("O");
+//     } else {
+//         console.log("A");
+//     }
+// } else {
+//     console.log("Better luck next time!");
+// }
---
+++ JS/practiceQ3.js
@@ -0,0 +1,55 @@
+// //  A good string that starts with letter A and has length > 3. WOP to find if string is good or not
+// let String = "app";
+
+// if ((String[0] == 'a') && (String.length>3)) {
+//     console.log("string is good");
+// } else {
+//     console.log("string is not good");
+// }
+
+
+// let num = 12;
+
+// if ((num%3 == 0) && ( (num+1 == 15) || (num-1 == 11) )) {
+//     console.log("safe");
+// } else {
+//     console.log("unsafe");
+// }
+
+// // use switch statements to print the day of the week usding number variable 'day' with values 1 to 7.
+// let day = 9;
+
+// switch (day) {
+//     case 1: 
+//     console.log("Monday")
+//         break;
+//     case 2: 
+//     console.log("Tuesday")
+//         break;
+//     case 3: 
+//     console.log("Wednesday")
+//         break;
+//     case 4: 
+//     console.log("Thursday")
+//         break;
+//     case 5: 
+//     console.log("Friday")
+//         break;
+//     case 6: 
+//     console.log("Saturday")
+//         break;
+//     case 7: 
+//     console.log("Sunday")
+//         break;
+
+//     default: console.log("oops not a day for you!")
+//         break;
+// }
+
+
+// // alert("Hacked!")  alert msg
+// console.error("this is an error msg");  //error msg
+// console.warn("this is an warning msg");   //warning msg
+
+// let roll = prompt("enter you roll number:");
+// console.log(roll)
