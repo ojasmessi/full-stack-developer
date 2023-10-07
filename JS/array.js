39a296ecd44a012ef99935218a31e5a6b923af44
Ojas Patil <oja2001pat@gmail.com>
2023-10-07 23:55:56 +0530

    array

--- JS/array.js
+++ JS/array.js
@@ -1,13 +1,13 @@
-let names = ["aman", "ojas", "anju"];
-let marks = [25,84,90];
-let info = [23, "raju", 90.2];
+// let names = ["aman", "ojas", "anju"];
+// let marks = [25,84,90];
+// let info = [23, "raju", 90.2];
 
-let fruits = ["mango" , "apple", "litchi"];
-fruits[0] = "banana";
+// let fruits = ["mango" , "apple", "litchi"];
+// fruits[0] = "banana";
 
 
-//Q1
-let start = ['january', 'july', 'march', 'august'];
+// //Q1
+// let start = ['january', 'july', 'march', 'august'];
 
-//Q2
-let arr = [["X", null, "O"], [null, "X", null], ['O', null, 'X']];
+// //Q2
+// let arr = [["X", null, "O"], [null, "X", null], ['O', null, 'X']];
---
+++ JS/practiceQ6.js
@@ -0,0 +1,11 @@
+let arr = [7, 9, 0, -2]; 
+let n=3;
+// let ans=array.slice(0,n);
+let ans = arr.slice(arr.length-n);
+
+let str = prompt("enter string");
+if (length==0) {
+    console.log("string is empty")
+} else {
+    console.log("string is not empty")
+}
