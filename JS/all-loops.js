357d8b010893ce90a974e029897cfa310f28801e
Ojas Patil <oja2001pat@gmail.com>
2023-10-08 20:20:09 +0530

    loops update to-do-app

--- JS/all-loops.js
+++ JS/all-loops.js
@@ -1,68 +1,68 @@
 
-//LOOP
-// // for(let i = 10; i<=5; i--){
+// //LOOP
+// // // for(let i = 10; i<=5; i--){
+// // //     console.log(i);
+// // // }
+// // console.log("Odd numbers");
+// // for(let i=1; i<=15; i=i+2){
 // //     console.log(i);
 // // }
-// console.log("Odd numbers");
-// for(let i=1; i<=15; i=i+2){
-//     console.log(i);
-// }
 
 
-// console.log("Even numbers");
-// for(let i=2; i<=14; i=i+2){
-//     console.log(i);
-// }
+// // console.log("Even numbers");
+// // for(let i=2; i<=14; i=i+2){
+// //     console.log(i);
+// // }
 
-// console.log("Tables");
-// let n = prompt("Enter the number for the table:");
-// n = parseInt(n);
-// for (let i = n; i <=n*10; i=i+n) {
-//     console.log(i);
+// // console.log("Tables");
+// // let n = prompt("Enter the number for the table:");
+// // n = parseInt(n);
+// // for (let i = n; i <=n*10; i=i+n) {
+// //     console.log(i);
     
-// }
+// // }
 
-// //NESTED LOOP
-// for(let i=1; i<=3; i++ ){
-//     for(let j=1; j<=3; j++){
-//         console.log(j);
-//     }
-// }
+// // //NESTED LOOP
+// // for(let i=1; i<=3; i++ ){
+// //     for(let j=1; j<=3; j++){
+// //         console.log(j);
+// //     }
+// // }
 
 
-// //WHILE LOOP
-// let i = 1;
-// while (i<=5) {
-//     console.log(i)
-// }
+// // //WHILE LOOP
+// // let i = 1;
+// // while (i<=5) {
+// //     console.log(i)
+// // }
 
-// //BREAK
-// const favmovie = "don";
+// // //BREAK
+// // const favmovie = "don";
 
-// let guess = prompt("Guess the movie:");
+// // let guess = prompt("Guess the movie:");
 
-// while (guess != favmovie) {
-//     if (guess == "quit") {
-//         console.log("you quit")
-//         break;
-//     }
-//     guess = prompt("wrong guess.try again!");
+// // while (guess != favmovie) {
+// //     if (guess == "quit") {
+// //         console.log("you quit")
+// //         break;
+// //     }
+// //     guess = prompt("wrong guess.try again!");
     
+// // }
+
+// // if (guess == favmovie) {
+// //     console.log("congo!")
+// // }
+
+
+// //LOOP WITH ARRAY
+// let fruits = ["banana", "orange", "apple", "litchi", "watermelon"];
+// fruits.push("pineapple");
+
+// // for (let i = 0; i < fruits.length; i++) {
+// //     console.log(i, fruits[i]);
+// //     }
+
+// for (fruit of fruits) {
+//     console.log(fruit);
 // }
-
-// if (guess == favmovie) {
-//     console.log("congo!")
-// }
-
-
-//LOOP WITH ARRAY
-let fruits = ["banana", "orange", "apple", "litchi", "watermelon"];
-fruits.push("pineapple");
-
-// for (let i = 0; i < fruits.length; i++) {
-//     console.log(i, fruits[i]);
-//     }
-
-for (fruit of fruits) {
-    console.log(fruit);
-}
