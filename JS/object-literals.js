e7abeb8ad071a6dd6b4dcb01b5c372564c1ad811
Ojas Patil <oja2001pat@gmail.com>
2023-10-10 19:38:28 +0530

    updated

---
+++ JS/object-literals.js
@@ -0,0 +1,36 @@
+let student = {
+    name : "ojas",
+    age : 21,
+    marks : 85,
+    city: ["mumbai", "delhi"]
+};
+
+
+//  add/update values
+const student1 = {
+    city : "Delhi",
+    age: 23,
+    name : "ojas"
+};
+
+
+//Array of objects
+const info = [
+    {
+    name:"ojas",
+    age: 21,                   // object 1
+    city: "mumbai"
+},
+
+{
+    name: "anju",
+    age: 23,                 // object 2
+    city: "haryana"
+},
+
+{
+    name: "raju",
+    age: 34,                 // object 3
+    city: "pune"
+}
+]
