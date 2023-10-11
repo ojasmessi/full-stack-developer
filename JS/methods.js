524fe8f178967200abc01b65688a6389ae953d98
Ojas Patil <oja2001pat@gmail.com>
2023-10-11 23:07:17 +0530

    methods

---
+++ JS/methods.js
@@ -0,0 +1,13 @@
+// object literals for functions is called METHODS
+
+const calculator = {
+    add: function(a,b) {    // function 1
+        return a+b;
+    },
+    sub: function(a,b) {         // function 2
+        return a-b;
+    },
+    mul: function(a,b) {       // function 3
+        return a*b;
+    }
+}
