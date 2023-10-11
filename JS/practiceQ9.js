e67f66113a29ed61fc14505488d606659aee6a33
Ojas Patil <oja2001pat@gmail.com>
2023-10-11 13:17:44 +0530

    Q9

---
+++ JS/practiceQ9.js
@@ -0,0 +1,23 @@
+//Q1 random dice roll
+let dice = Math.floor(Math.random() * 6) + 1;
+
+console.log(dice);
+
+//Q2 object literals car details
+let car = {
+    name: 'BMW',
+    model: 'X5',
+    color: 'black'
+}
+console.log(car);
+
+//Q3 object literals person details
+let person = {
+    name: 'ojas',
+    age: '22',
+    city: 'Mumbai'
+}
+
+person.city = 'New York';
+person.country = 'USA';
+console.log(person);
