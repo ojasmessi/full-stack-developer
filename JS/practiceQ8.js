38c065a3b9e362b8e442af0a31e195a45cd7a93b
Ojas Patil <oja2001pat@gmail.com>
2023-10-11 13:06:31 +0530

    guessing number game

---
+++ JS/practiceQ8.js
@@ -0,0 +1,26 @@
+//Guessing Game
+
+const max = prompt("enter max number");
+
+const random = Math.floor(Math.random() * max) + 1;
+
+let guess = prompt("guess random number");
+
+while (true) {
+    if (guess == "quit") {
+        console.log("you quit");
+        break;
+    }
+
+    if (guess == random) {
+        console.log("you guessed it right! the number is", random);
+        break;
+    } else if (guess < random) {
+        guess = prompt("the number was too small");
+    } else {
+        guess = prompt("the number was too large");
+    }
+    // else {
+    //     guess = prompt("your guess is wrong. please try again!")
+    // }
+}
