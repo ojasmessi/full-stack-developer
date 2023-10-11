beb54f450d82b35cedce7b9ff54e0250cc5ee064
Ojas Patil <oja2001pat@gmail.com>
2023-10-11 13:08:16 +0530

    guessing game

--- JS/practiceQ8.js
+++ JS/practiceQ8.js
@@ -1,26 +1,26 @@
-//Guessing Game
+// //Guessing Game
 
-const max = prompt("enter max number");
+// const max = prompt("enter max number");
 
-const random = Math.floor(Math.random() * max) + 1;
+// const random = Math.floor(Math.random() * max) + 1;
 
-let guess = prompt("guess random number");
+// let guess = prompt("guess random number");
 
-while (true) {
-    if (guess == "quit") {
-        console.log("you quit");
-        break;
-    }
+// while (true) {
+//     if (guess == "quit") {
+//         console.log("you quit");
+//         break;
+//     }
 
-    if (guess == random) {
-        console.log("you guessed it right! the number is", random);
-        break;
-    } else if (guess < random) {
-        guess = prompt("the number was too small");
-    } else {
-        guess = prompt("the number was too large");
-    }
-    // else {
-    //     guess = prompt("your guess is wrong. please try again!")
-    // }
-}
+//     if (guess == random) {
+//         console.log("you guessed it right! the number is", random);
+//         break;
+//     } else if (guess < random) {
+//         guess = prompt("the number was too small");
+//     } else {
+//         guess = prompt("the number was too large");
+//     }
+//     // else {
+//     //     guess = prompt("your guess is wrong. please try again!")
+//     // }
+// }
