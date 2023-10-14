ff9dbff0dacf888438c875d2e80adbccc175d254
Ojas Patil <oja2001pat@gmail.com>
2023-10-14 20:31:39 +0530

    Q10

---
+++ JS/practiceQ10.js
@@ -0,0 +1,12 @@
+// let arr = [8,5,22,7,6,8,1];
+// let num = 5;
+
+// function getElements(arr,num) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > num){
+//         console.log(arr[i]);
+//     }
+// }
+// }
+
+// getElements(arr,num);
