5fa78757450216c740ad844b981ee9a77460777e
Ojas Patil <oja2001pat@gmail.com>
2023-10-15 17:59:33 +0530

    updated

---
+++ JS/practiceQ11.js
@@ -0,0 +1,10 @@
+//Q1
+
+let number = [1,2,3,4,5,6];
+
+const square = nums.map((num) => num * num);
+console.log(square);
+
+let sum = square.reduce((acc,cur) => acc + num , 0 );
+let avg = sum / nums.length;
+console.log(avg);
