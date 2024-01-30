883eb66984d8aae356b8d2a035753b745b195d30
Ojas Patil <oja2001pat@gmail.com>
2024-01-30 20:00:04 +0530

    node.js

---
+++ BACKEND/node.js
@@ -0,0 +1,13 @@
+// let n = 5;
+// for (let i = 0; i < 5; i++) {
+//     console.log("hello", i);
+
+// }
+// console.log("bye");
+
+// console.log(process.argv);
+
+const someValue = require("./export");
+
+
+console.log(someValue);
